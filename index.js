c69b105682d67645700d2cb40c1eeae65afa9038
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-24 19:51:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function sayHiToGrandma(string) {
   else if (string.toUpperCase() === uppercase)
   return "YES INDEED!"
   else (string === "I love you, Grandma.")
-  return "I love you, too"
+  return "I love you, too."
 }
